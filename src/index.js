f7de7123733da0bf7a6546cbaf6a7432bea00879
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-08-06 21:42:10 -0400

    priceline

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { module } from "angular";
 import React from "react";
 import { react2angular } from "react2angular";
+import { ThemeProvider } from "pcln-design-system";
 
 const Box = () => <h1>hello world!!</h1>;
 
@@ -8,4 +9,5 @@ module("HelloWorldApp", [])
   .controller("HelloWorldController", function($scope) {
     $scope.greeting = "Hello World";
   })
-  .component("box", react2angular(Box));
+  .component("box", react2angular(Box))
+  .component("themeProvider", react2angular(ThemeProvider));

7e13469b383fdc9068bd3e7039e894160907deb2
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-08-08 15:09:48 -0400

    hmm.. working

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import { module } from "angular";
+import {module} from "angular";
 import React from "react";
-import { react2angular } from "react2angular";
-import { ThemeProvider } from "pcln-design-system";
+import {react2angular} from "react2angular";
+import {ThemeProvider, Text} from "pcln-design-system";
 
 const Box = () => <h1>hello world!!</h1>;
+const MyText = () => <Text color="blue">hmm..</Text>;
 
 module("HelloWorldApp", [])
-  .controller("HelloWorldController", function($scope) {
-    $scope.greeting = "Hello World";
-  })
-  .component("box", react2angular(Box))
-  .component("themeProvider", react2angular(ThemeProvider));
+    .controller("HelloWorldController", function ($scope) {
+        $scope.greeting = "Hello World";
+    })
+    .component("box", react2angular(Box))
+    .component("themeProvider", react2angular(ThemeProvider))
+    .component("myText", react2angular(MyText, ['color']));

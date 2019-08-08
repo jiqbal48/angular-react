f76ba38c9d4647e59287842fdbd6fd223e07dd9e
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-08-08 15:41:15 -0400

    hm... this lib is not too powerful

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import React from "react";
 import {react2angular} from "react2angular";
 import {ThemeProvider, Text} from "pcln-design-system";
 
-const Box = () => <h1>hello world!!</h1>;
+
 const MyText = () => <Text color="blue">hmm..</Text>;
+const Box = (props) => {
+    console.log('props.color:', props);
+    return (<h1 style={{color: props.color}}>what a wonderful world</h1>);
+};
+
 
 module("HelloWorldApp", [])
     .controller("HelloWorldController", function ($scope) {
         $scope.greeting = "Hello World";
     })
-    .component("box", react2angular(Box))
+    .component("box", react2angular(Box, ['style']))
     .component("themeProvider", react2angular(ThemeProvider))
     .component("myText", react2angular(MyText, ['color']));

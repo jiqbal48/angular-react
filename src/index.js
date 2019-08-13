a6c0bbc53888415cce24a272e7001c67d49c90c1
Jahangir Iqbal <jiqbal@vibrenthealth.com>
2019-08-13 13:33:50 -0400

    priceline

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import {module} from "angular";
 import React from "react";
 import {react2angular} from "react2angular";
-import {ThemeProvider, Text} from "pcln-design-system";
+import {ThemeProvider, Box, Text} from "pcln-design-system";
 
-const Box = () => <h1>hello world!!</h1>;
-const MyText = () => <Text color="blue">hmm..</Text>;
+const controller = ($scope) => {
+    $scope.data = {url: 'https://news.ycombinator.com'};
+};
+
+
+const SurveyCards = ({config}) => {
+    console.log('config:', config);
+    return (
+        <ThemeProvider>
+            <Box p={3} bg='#589167'>
+                <Text color="blue">hi no config</Text>
+            </Box>
+        </ThemeProvider>
+    );
+};
+
 
 module("HelloWorldApp", [])
-    .controller("HelloWorldController", function ($scope) {
-        $scope.greeting = "Hello World";
-    })
-    .component("box", react2angular(Box))
-    .component("themeProvider", react2angular(ThemeProvider))
-    .component("myText", react2angular(MyText, ['color']));
+    .controller("HelloWorldController", controller)
+    .component("surveyCards", react2angular(SurveyCards, ['config']));
